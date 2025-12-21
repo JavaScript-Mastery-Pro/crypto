0fbe8a4aacb01ad5663b7da25a017e42b35abc39
sujatagunale <sujata@jsmastery.pro>
2025-12-21 10:57:08 +0530

    refactor: converter styles

--- components/coin-details/Converter.tsx
+++ components/coin-details/Converter.tsx
@@ -16,61 +16,55 @@ export const Converter = ({ symbol, icon, priceList }: ConverterProps) => {
   const [currency, setCurrency] = useState('usd');
   const [amount, setAmount] = useState('10');
 
-  // Calculate converted price
   const convertedPrice = (parseFloat(amount) || 0) * (priceList[currency] || 0);
 
   return (
-    <div className='w-full space-y-5'>
-      <h4 className='converter-title'>{symbol.toUpperCase()} Converter</h4>
-      <div className='converter-container'>
-        <div className='converter-input-wrapper'>
+    <div id='converter'>
+      <h4>{symbol.toUpperCase()} Converter</h4>
+
+      <div className='panel'>
+        <div className='input-wrapper'>
           <Input
             type='number'
             placeholder='Amount'
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
-            className='converter-input'
+            className='input'
           />
-          <div className='converter-coin-info'>
+          <div className='coin-info'>
             <Image src={icon} alt={symbol} width={20} height={20} />
-            <p className='converter-coin-symbol'>{symbol.toUpperCase()}</p>
+            <p>{symbol.toUpperCase()}</p>
           </div>
         </div>
 
-        <div>
-          <div className='converter-divider-wrapper'>
-            <div className='converter-divider-line' />
-            <Image
-              src='/assets/converter.svg'
-              alt='converter'
-              width={32}
-              height={32}
-              className='converter-icon'
-            />
-          </div>
+        <div className='divider'>
+          <div className='line' />
+
+          <Image
+            src='/assets/converter.svg'
+            alt='converter'
+            width={32}
+            height={32}
+            className='icon'
+          />
         </div>
 
-        <div className='converter-output-wrapper'>
-          <p className='text-base font-medium'>
-            {formatPrice(convertedPrice, 2, currency, false)}
-          </p>
+        <div className='output-wrapper'>
+          <p>{formatPrice(convertedPrice, 2, currency, false)}</p>
+
           <Select value={currency} onValueChange={setCurrency}>
-            <SelectTrigger
-              className='converter-select-trigger'
-              value={currency}
-            >
-              <SelectValue placeholder='Select' className='flex gap-2'>
-                <span className='converter-currency'>
-                  {currency.toUpperCase()}
-                </span>
+            <SelectTrigger className='select-trigger' value={currency}>
+              <SelectValue placeholder='Select' className='select-value'>
+                {currency.toUpperCase()}
               </SelectValue>
             </SelectTrigger>
-            <SelectContent className='converter-select-content'>
+
+            <SelectContent className='select-content' data-converter>
               {Object.keys(priceList).map((currencyCode) => (
                 <SelectItem
                   key={currencyCode}
                   value={currencyCode}
-                  className='converter-select-item'
+                  className='select-item'
                 >
                   {currencyCode.toUpperCase()}
                 </SelectItem>
