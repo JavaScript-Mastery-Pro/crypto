079616e5738b42b1ca7094d051518da31808e9f3
sujatagunale <sujata@jsmastery.pro>
2025-12-21 23:32:57 +0530

    fix: fallback layout

--- components/home/Fallback.tsx
+++ components/home/Fallback.tsx
@@ -1,67 +1,77 @@
+import { DataTable } from '@/components/DataTable';
 import { Skeleton } from '../ui/skeleton';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '../ui/table';
 
 export const CategoriesFallback = () => (
   <div id='categories-fallback' className='custom-scrollbar'>
     <h4>Top Categories</h4>
-    <Table>
-      <TableHeader className='header'>
-        <TableRow className='header-row'>
-          <TableHead className='head head-left'>Category</TableHead>
-          <TableHead className='head'>Top Gainers</TableHead>
-          <TableHead className='head head-change'>24h Change</TableHead>
-          <TableHead className='head'>Market Cap</TableHead>
-          <TableHead className='head'>24h Volume</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {Array.from({ length: 6 }).map((_, index) => (
-          <TableRow key={index} className='row'>
-            <TableCell className='category-cell'>
-              <Skeleton className='skeleton category-skeleton' />
-            </TableCell>
-            <TableCell className='gainers-cell'>
-              {Array.from({ length: 3 }).map((__, coinIndex) => (
-                <Skeleton
-                  key={coinIndex}
-                  className='skeleton coin-skeleton'
-                />
-              ))}
-            </TableCell>
-            <TableCell className='value-cell'>
+    <DataTable
+      tableClassName='mt-3'
+      columns={[
+        {
+          header: 'Category',
+          cellClassName: 'category-cell',
+          cell: () => <Skeleton className='skeleton category-skeleton' />,
+        },
+        {
+          header: 'Top Gainers',
+          cellClassName: 'top-gainers-cell',
+          cell: () =>
+            Array.from({ length: 3 }).map((_, coinIndex) => (
+              <Skeleton key={coinIndex} className='skeleton coin-skeleton' />
+            )),
+        },
+        {
+          header: '24h Change',
+          cellClassName: 'change-header-cell',
+          cell: () => (
+            <div className='change-cell'>
               <Skeleton className='skeleton value-skeleton-sm' />
-            </TableCell>
-            <TableCell className='value-cell'>
-              <Skeleton className='skeleton value-skeleton-md' />
-            </TableCell>
-            <TableCell className='value-cell'>
-              <Skeleton className='skeleton value-skeleton-lg' />
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+              <Skeleton className='skeleton change-icon' />
+            </div>
+          ),
+        },
+        {
+          header: 'Market Cap',
+          cellClassName: 'market-cap-cell',
+          cell: () => <Skeleton className='skeleton value-skeleton-md' />,
+        },
+        {
+          header: '24h Volume',
+          cellClassName: 'volume-cell',
+          cell: () => <Skeleton className='skeleton value-skeleton-lg' />,
+        },
+      ]}
+      data={Array.from({ length: 6 })}
+      rowKey={(_, index) => index}
+    />
   </div>
 );
 
 export const CoinOverviewFallback = () => (
   <div id='coin-overview-fallback'>
-    <div className='panel'>
-      <div className='header'>
-        <Skeleton className='skeleton header-image' />
-        <div className='header-lines'>
-          <Skeleton className='skeleton header-line-sm' />
-          <Skeleton className='skeleton header-line-lg' />
+    <div id='candlestick-chart' className='is-fallback'>
+      <div className='chart-header'>
+        <div className='flex-1'>
+          <div className='header'>
+            <Skeleton className='skeleton header-image' />
+            <div className='info'>
+              <Skeleton className='skeleton header-line-sm' />
+              <Skeleton className='skeleton header-line-lg' />
+            </div>
+          </div>
+        </div>
+        <div className='button-group'>
+          {Array.from({ length: 7 }).map((_, index) => (
+            <Skeleton
+              key={index}
+              className='skeleton period-button-skeleton'
+            />
+          ))}
         </div>
       </div>
-      <Skeleton className='skeleton chart-skeleton' />
+      <div className='chart'>
+        <Skeleton className='skeleton chart-skeleton' />
+      </div>
     </div>
   </div>
 );
@@ -69,34 +79,39 @@ export const CoinOverviewFallback = () => (
 export const TrendingCoinsFallback = () => (
   <div id='trending-coins-fallback'>
     <h4>Trending Coins</h4>
-    <div className='table-wrapper custom-scrollbar'>
-      <Table>
-        <TableHeader className='header'>
-          <TableRow className='header-row'>
-            <TableHead className='head head-left'>Name</TableHead>
-            <TableHead className='head'>24h Change</TableHead>
-            <TableHead className='head head-right'>Price</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {Array.from({ length: 6 }).map((_, index) => (
-            <TableRow key={index} className='row'>
-              <TableCell className='name-cell'>
-                <div className='name'>
-                  <Skeleton className='skeleton name-image' />
-                  <Skeleton className='skeleton name-line' />
-                </div>
-              </TableCell>
-              <TableCell className='change-cell'>
-                <Skeleton className='skeleton change-line' />
-              </TableCell>
-              <TableCell className='price-cell'>
-                <Skeleton className='skeleton price-line' />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
+    <DataTable
+      tableClassName='trending-coins-table mt-3'
+      headerCellClassName='py-3!'
+      bodyCellClassName='py-2!'
+      columns={[
+        {
+          header: 'Name',
+          cellClassName: 'name-cell',
+          cell: () => (
+            <div className='name-link'>
+              <Skeleton className='skeleton name-image' />
+              <Skeleton className='skeleton name-line' />
+            </div>
+          ),
+        },
+        {
+          header: '24h Change',
+          cellClassName: 'change-cell',
+          cell: () => (
+            <div className='price-change'>
+              <Skeleton className='skeleton change-line' />
+              <Skeleton className='skeleton change-icon' />
+            </div>
+          ),
+        },
+        {
+          header: 'Price',
+          cellClassName: 'price-cell',
+          cell: () => <Skeleton className='skeleton price-line' />,
+        },
+      ]}
+      data={Array.from({ length: 6 })}
+      rowKey={(_, index) => index}
+    />
   </div>
 );
